80fcd4121a4fcd62dfa04c2cc3b7d46813f92326
Aldragz <kevinsaputra.0rb@gmail.com>
2025-12-13 19:28:15 +0700

    Update sepik2.sql

--- database/sepik2.sql
+++ database/sepik2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2025 at 05:08 PM
+-- Generation Time: Dec 13, 2025 at 12:58 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
